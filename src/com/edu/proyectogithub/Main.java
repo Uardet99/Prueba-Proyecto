de7db14a9f8d9318c639cac29b6977a184749292
Edu Valls Gisbert <eduardvalls17@gmail.com>
2023-02-20 23:26:04 +0100

    Hastraakejbf

--- src/com/edu/proyectogithub/Main.java
+++ src/com/edu/proyectogithub/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola PEPE");
-
+		System.out.println("Adeu Pepe");
 	}
-	public static void main2(String[] args) {
-		System.out.println("Adios");
-
-	}
+	
 }
