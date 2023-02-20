479f8e0b8a01793e3141a8d54e88ddeebe9cf62b
Adrian <adri7agu@gmail.com>
2023-02-20 23:22:38 +0100

    test2

--- src/com/edu/proyectogithub/Main.java
+++ src/com/edu/proyectogithub/Main.java
@@ -3,7 +3,7 @@ package com.edu.proyectogithub;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Edu");
+		System.out.println("Hola PEPE");
 
 	}
 	public static void main2(String[] args) {
